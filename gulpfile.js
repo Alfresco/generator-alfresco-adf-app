b87ced6e4fbd5610506bc6a33a841ddb366caed1
Mario Romano <dnamario@hotmail.com>
2016-06-29 15:34:43 +0100

    #284 coveralls integration

--- gulpfile.js
+++ gulpfile.js
@@ -56,4 +56,4 @@ gulp.task('coveralls', ['test'], function () {
 });
 
 gulp.task('prepublish', ['nsp']);
-gulp.task('default', ['static', 'test']);
+gulp.task('default', ['static', 'test', 'coveralls']);
