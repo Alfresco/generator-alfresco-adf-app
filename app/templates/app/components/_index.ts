f87ae62809fa302b88cab7e8bf4a6683ec894215
Eugenio Romano <eugenio.romano@alfresco.com>
2016-12-19 18:45:29 +0000

    #42 Update the generator to the version 1.0.0 of ADF

--- app/templates/app/components/_index.ts
+++ app/templates/app/components/_index.ts
@@ -1,15 +1,22 @@
 <%- licenseHeader %>
+
 export { LoginDemoComponent } from './login/login-demo.component';
 export { SettingComponent } from './setting/setting.component';
+export { HomeComponent } from './home/home.component';
+export { AboutComponent } from './about/about.component';
 <% if (contentPage == true) { %>
 export { FilesComponent } from './files/files.component';
 <% } %>
 <% if (searchBar == true) { %>
 export { SearchComponent } from './search/search.component';
-<% } %>
-<% if (searchBar == true) { %>
 export { SearchBarComponent } from './search/search-bar.component';
 <% } %>
 <% if (bpmTaskPage == true) { %>
-export { ActivitiDemoComponent } from './tasks/activiti-demo.component';
+export { FormNodeViewer } from './activiti/form-node-viewer.component';
+export { ActivitiDemoComponent } from './activiti/activiti-demo.component';
+export { FormViewer } from './activiti/form-viewer.component';
+export { ActivitiAppsView } from './activiti/apps.view';
 <% } %>
+
+
+
