8aa47700030eaea9898997d7e07d5548b11e9b29
Mario Romano <dnamario@hotmail.com>
2017-01-27 18:50:25 +0000

    #57 update to adf 1.1.0

--- app/templates/app/components/about/_about.component.ts
+++ app/templates/app/components/about/_about.component.ts
@@ -3,40 +3,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 import { ObjectDataTableAdapter } from 'ng2-alfresco-datatable';
+import { LogService } from 'ng2-alfresco-core';
 
 @Component({
-    selector: 'about-page',
-    templateUrl: './about.component.html'
+  selector: 'about-page',
+  templateUrl: './about.component.html'
 })
 export class AboutComponent implements OnInit {
 
-    data: ObjectDataTableAdapter;
+  data: ObjectDataTableAdapter;
 
-    constructor(private http: Http) {
-    }
+  constructor(private http: Http,
+              private logService: LogService) {
+  }
 
-    ngOnInit() {
-        this.http.get('/versions.json').subscribe(response => {
-            var regexp = new RegExp("^(ng2-activiti|ng2-alfresco|alfresco-)", 'g');
+  ngOnInit() {
+    this.http.get('/versions.json').subscribe(response => {
+      let regexp = new RegExp('^(ng2-activiti|ng2-alfresco|alfresco-)', 'g');
 
-            var alfrescoPackages = Object.keys(response.json().dependencies).filter(function (val) {
-                console.log(val);
-                return regexp.test(val);
-            });
+      let alfrescoPackages = Object.keys(response.json().dependencies).filter((val) => {
+        this.logService.log(val);
+        return regexp.test(val);
+      });
 
-            let  alfrescoPackagesTableRappresentation = [];
-            alfrescoPackages.forEach((val)=> {
-                console.log(response.json().dependencies[val]);
-                alfrescoPackagesTableRappresentation.push({name:val,version:response.json().dependencies[val].version});
-            });
+      let alfrescoPackagesTableRappresentation = [];
+      alfrescoPackages.forEach((val) => {
+        this.logService.log(response.json().dependencies[val]);
+        alfrescoPackagesTableRappresentation.push({name: val, version: response.json().dependencies[val].version});
+      });
 
-            console.log(alfrescoPackagesTableRappresentation);
+      this.logService.log(alfrescoPackagesTableRappresentation);
 
-            this.data = new ObjectDataTableAdapter(alfrescoPackagesTableRappresentation, [
-                {type: 'text', key: 'name', title: 'Name', sortable: true},
-                {type: 'text', key: 'version', title: 'Version', sortable: true}
-            ]);
-        });
-
-    }
+      this.data = new ObjectDataTableAdapter(alfrescoPackagesTableRappresentation, [
+        {type: 'text', key: 'name', title: 'Name', sortable: true},
+        {type: 'text', key: 'version', title: 'Version', sortable: true}
+      ]);
+    });
+  }
 }
