17fe3c019d82074d19d55821059c44a4a2b598f3
Eugenio Romano <eromano@users.noreply.github.com>
2017-05-25 11:12:47 +0100

    update to 1.5.0 (#85)
    * update to 1.5.0
    
    * remove increase size memory

---
+++ app/templates/config/_webpack.prod.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const commonConfig = require('./webpack.common.js');
+const helpers = require('./helpers');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = webpackMerge(commonConfig, {
+
+  devtool: 'source-map',
+
+  output: {
+    path: helpers.root('dist'),
+    publicPath: '/',
+    filename: '[name].[hash].js',
+    chunkFilename: '[id].[hash].chunk.js'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js'],
+    modules: [helpers.root('node_modules')]
+  },
+
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
+        mangle: {
+            keep_fnames: true
+        },
+        compress: {
+            warnings: false
+        },
+        output: {
+            comments: false
+        },
+        sourceMap: true
+    }),
+    new ExtractTextPlugin('[name].[hash].css'),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringify(ENV)
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      htmlLoader: {
+        minimize: false // workaround for ng2
+      }
+    })
+  ]
+});
