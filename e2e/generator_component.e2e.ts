b0a7bece0465afe27737f26237c3ec469229ad1c
Eugenio Romano <eromano@users.noreply.github.com>
2018-10-19 10:13:32 +0100

    automation test viewer (#168)
    * test viewer
    
    * update webdriver
    
    * fix travis
    
    * build dist
    
    * fix folder dist
    
    * fix script
    
    * add install
    
    * update test
    
    * proxy settings
    
    * try use different conf
    
    * use default os
    
    * try different protractor configutation for travis
    
    * log
    
    * more log
    
    * try delete node_modules
    
    * fix travis configuration aps
    
    * remove cache
    
    * fix travis script
    
    * limit test executed on APS template
    
    * fix travis

---
+++ e2e/generator_component.e2e.ts
@@ -0,0 +1,75 @@
+/*!
+ * @license
+ * Copyright 2016 Alfresco Software, Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { browser } from 'protractor';
+
+import TestConfig = require('./test.config');
+
+import { ViewerPage } from './viewerPage';
+import { LoginPage } from './loginPage';
+
+import AlfrescoApi = require('alfresco-js-api-node');
+import { UploadActions } from './upload.actions';
+
+describe('Content Services Viewer', () => {
+
+  let viewerPage = new ViewerPage();
+  let loginPage = new LoginPage();
+
+  let pdfFile = {
+    'name': 'a_file_supported.pdf',
+    'firstPageText': 'A Journey into Test Frameworks'
+  };
+
+  let loginURL = TestConfig.adf.url + TestConfig.adf.port + '/login';
+
+  beforeAll(async (done) => {
+    let uploadActions = new UploadActions();
+
+    this.alfrescoJsApi = new AlfrescoApi({
+      provider: 'ECM',
+      hostEcm: TestConfig.adf.url
+    });
+
+    await this.alfrescoJsApi.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
+
+    let pdfFileUploaded = await uploadActions.uploadFile(this.alfrescoJsApi, '/pdf_file.pdf', 'pdf_file.pdf', '-my-');
+    Object.assign(pdfFile, pdfFileUploaded.entry);
+
+    browser.driver.get(loginURL);
+
+    loginPage.login(TestConfig.adf.adminEmail, TestConfig.adf.adminPassword);
+
+    done();
+  });
+
+  it('[C260498] Check viewer for a generated app', () => {
+
+    let viewerUrl = TestConfig.adf.url + TestConfig.adf.port + `/documentlist(overlay:files/${pdfFile.id}/view)`;
+
+    browser.driver.get(viewerUrl);
+
+    browser.driver.sleep(3000); // wait open file
+
+    viewerPage.checkFileContent('1', pdfFile.firstPageText);
+    viewerPage.checkCloseButtonIsDisplayed();
+
+
+    viewerPage.clickCloseButton();
+  });
+
+});
