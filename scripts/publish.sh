d991916b73a23eff3224a9222fb913c37bc67c9e
Eugenio Romano <eromano@users.noreply.github.com>
2020-02-07 00:44:02 +0000

    Update publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 set -e
-touch ./dist/package/.npmrc
-echo 'strict-ssl=false' >> ./dist/package/.npmrc
-echo 'registry=http://${NPM_REGISTRY_ADDRESS}' >> ./dist/package/.npmrc
-echo '//${NPM_REGISTRY_ADDRESS}/:_authToken="${NPM_REGISTRY_TOKEN}"' >> ./dist/package/.npmrc
+touch .npmrc
+echo 'strict-ssl=false' >> .npmrc
+echo 'registry=http://${NPM_REGISTRY_ADDRESS}' >> .npmrc
+echo '//${NPM_REGISTRY_ADDRESS}/:_authToken="${NPM_REGISTRY_TOKEN}"' >> .npmrc
 
 npm install
 npm publish
