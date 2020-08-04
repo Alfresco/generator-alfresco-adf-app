cb4d8593681b87415172639838a6653f493e5f66
Cano <david.cano.nieto@gmail.com>
2020-08-04 09:04:44 +0100

    Fix component path

--- app/templates/adf-cli-acs-template/src/app/app.routes.ts
+++ app/templates/adf-cli-acs-template/src/app/app.routes.ts
@@ -19,7 +19,7 @@ import { Routes } from '@angular/router';
 import { AuthGuardEcm } from '@alfresco/adf-core';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { DocumentlistComponent } from './document-list/document-list.component';
+import { DocumentlistComponent } from './documentlist/documentlist.component';
 import { AppLayoutComponent } from './app-layout/app-layout.component';
 import { FileViewComponent } from './file-view/file-view.component';
 
