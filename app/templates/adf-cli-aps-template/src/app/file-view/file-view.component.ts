56947f1d6a88ff37d2b679c4ae72bf50b719ea6d
davidcanonieto <david.cano.nieto@gmail.com>
2020-11-13 11:40:07 +0000

    [MNT-21924] Fix Viewer freeze after closing. (#275)

--- app/templates/adf-cli-aps-template/src/app/file-view/file-view.component.ts
+++ app/templates/adf-cli-aps-template/src/app/file-view/file-view.component.ts
@@ -16,8 +16,8 @@
  */
 
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AlfrescoApiService } from '@alfresco/adf-core';
+import { ActivatedRoute, PRIMARY_OUTLET, Router } from '@angular/router';
+import { NodesApiService } from '@alfresco/adf-core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
@@ -33,17 +33,16 @@ export class FileViewComponent implements OnInit {
     constructor(private router: Router,
                 private route: ActivatedRoute,
                 private snackBar: MatSnackBar,
-                private apiService: AlfrescoApiService) {
+                private nodeApiService: NodesApiService) {
     }
 
     ngOnInit() {
-
         this.route.params.subscribe(params => {
             const id = params.nodeId;
             if (id) {
-                this.apiService.getInstance().nodes.getNodeInfo(id).then(
+                this.nodeApiService.getNode(id).subscribe(
                     (node) => {
-                        if (node) {
+                        if (node && node.isFile) {
                             this.nodeId = id;
                             return;
                         }
@@ -57,5 +56,10 @@ export class FileViewComponent implements OnInit {
 
     onUploadError(errorMessage: string) {
         this.snackBar.open(errorMessage, '', { duration: 4000 });
+    }
+
+    onViewerVisibilityChanged() {
+        const primaryUrl = this.router.parseUrl(this.router.url).root.children[PRIMARY_OUTLET].toString();
+        this.router.navigateByUrl(primaryUrl);
     }
 }
