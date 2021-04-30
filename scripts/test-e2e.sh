a45c8645e56d1da075133fdeb4eb369b2054db92
Eugenio Romano <eromano@users.noreply.github.com>
2021-04-30 09:51:42 +0100

    Update test-e2e.sh

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -144,9 +144,15 @@ rm -rf node_modules
 
 cd ../../../
 
-echo "webdriver update"
-
-./node_modules/protractor/node_modules/webdriver-manager/bin/webdriver-manager update --standalone false --gecko false
+echo "====== Update webdriver-manager ====="
+if [ "$CI" = "true" ]; then
+    export chrome=$(google-chrome --product-version)
+    echo "Updating wedriver-manager with chromedriver: $chrome."
+    ./node_modules/protractor/bin/webdriver-manager update --gecko=false --versions.chrome=$chrome
+else
+    echo "Updating wedriver-manager with latest chromedriver, be sure to use evergreen Chrome."
+    ./node_modules/protractor/bin/webdriver-manager update --gecko=false
+fi
 
 echo "RUN TEST E2E"
 
