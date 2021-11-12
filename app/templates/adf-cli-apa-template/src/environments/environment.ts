2fb6721cc8dfb51e04956895afdb413b9cd78813
Eugenio Romano <eugenio.romano@alfresco.com>
2021-11-12 16:33:14 +0000

    fix templates

--- app/templates/adf-cli-apa-template/src/environments/environment.ts
+++ app/templates/adf-cli-apa-template/src/environments/environment.ts
@@ -3,9 +3,6 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-import { ApplicationDeploymentService } from '../app/services/application-deployment.service';
-
 export const environment = {
-    production: false,
-    applicationDeploymentService: ApplicationDeploymentService
+    production: false
 };
