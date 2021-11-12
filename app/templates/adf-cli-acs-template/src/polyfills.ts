447fa702b3b724efa5b0668c52a09db8e2690fe0
Eugenio Romano <eugenio.romano@alfresco.com>
2021-11-12 16:10:22 +0000

    style fix polyfills fix travis 14 node for karma pin

--- app/templates/adf-cli-acs-template/src/polyfills.ts
+++ app/templates/adf-cli-acs-template/src/polyfills.ts
@@ -18,52 +18,18 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-import 'core-js/es6/symbol';
-import 'core-js/es6/object';
-import 'core-js/es6/function';
-import 'core-js/es6/parse-int';
-import 'core-js/es6/parse-float';
-import 'core-js/es6/number';
-import 'core-js/es6/math';
-import 'core-js/es6/string';
-import 'core-js/es6/date';
-import 'core-js/es6/array';
-import 'core-js/es6/regexp';
-import 'core-js/es6/map';
-import 'core-js/es6/weak-map';
-import 'core-js/es6/set';
-
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
-import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** Evergreen browsers require these. **/
-import 'core-js/es6/reflect';
-import 'core-js/es7/reflect';
-
-
-/**
- * Web Animations `@angular/platform-browser/animations`
- * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /***************************************************************************************************
  * Zone JS is required by Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
-
 /**
  * Support custom event in IE11
  *
- * */
-import 'custom-event-polyfill/custom-event-polyfill';  // Run `npm install --save custom-event-polyfill`.
+ */
+(window as any).global = window;
 
+// import 'custom-event-polyfill/custom-event-polyfill'; // Run `npm install --save custom-event-polyfill`.
