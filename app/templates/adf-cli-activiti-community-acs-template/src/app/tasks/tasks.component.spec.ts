f54fd5bd7dd25508e9c8b91b3c53df1125612d9d
Denys Vuika <denys.vuika@gmail.com>
2022-03-18 10:22:45 +0000

    cleanup useless test init

--- app/templates/adf-cli-activiti-community-acs-template/src/app/tasks/tasks.component.spec.ts
+++ app/templates/adf-cli-activiti-community-acs-template/src/app/tasks/tasks.component.spec.ts
@@ -1,44 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { ProcessListCloudService, ProcessServicesCloudModule, TaskListCloudService } from '@alfresco/adf-process-services-cloud';
-import { CoreModule, AppConfigService, AppConfigServiceMock, TranslationService, TranslationMock } from '@alfresco/adf-core';
+import { ProcessServicesCloudModule } from '@alfresco/adf-process-services-cloud';
+import { CoreModule } from '@alfresco/adf-core';
 import { TasksComponent } from './tasks.component';
-import { AlfrescoApiServiceMock, AlfrescoApiService } from '@alfresco/adf-core';
 import { TranslateModule } from '@ngx-translate/core';
-import { of } from 'rxjs';
 
 describe('TasksComponent', () => {
   let component: TasksComponent;
   let fixture: ComponentFixture<TasksComponent>;
-  let alfrescoApiService: AlfrescoApiService;
-  let taskListCloudService: TaskListCloudService;
-  let processListCloudService: ProcessListCloudService;
-
-  const fakeList = {
-    list: {
-      entries: [],
-      pagination: {
-        skipCount: 0,
-        maxItems: 15,
-        count: 0,
-        hasMoreItems: false,
-        totalItems: 0
-      }
-    }
-  };
-
-  const oauth2AuthMock = {
-    oauth2Auth: {
-      callCustomApi: () => {
-        return Promise.resolve({});
-      }
-    },
-    isEcmBpmConfiguration: () => false,
-    isEcmConfiguration: () => false,
-    isEcmLoggedIn: () => false,
-    reply: jasmine.createSpy('reply')
-  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -49,23 +19,11 @@ describe('TasksComponent', () => {
         CoreModule.forRoot(),
         ProcessServicesCloudModule
       ],
-      declarations: [TasksComponent],
-      providers: [
-        { provide: AppConfigService, useClass: AppConfigServiceMock },
-        { provide: AlfrescoApiService, useClass: AlfrescoApiServiceMock },
-        { provide: TranslationService, useClass: TranslationMock }
-      ]
+      declarations: [TasksComponent]
     });
 
     fixture = TestBed.createComponent(TasksComponent);
-    alfrescoApiService = TestBed.inject(AlfrescoApiService);
-    taskListCloudService = TestBed.inject(TaskListCloudService);
-    processListCloudService = TestBed.inject(ProcessListCloudService);
     component = fixture.componentInstance;
-    spyOn(alfrescoApiService, 'getInstance').and.returnValue(oauth2AuthMock);
-    spyOn(taskListCloudService, 'getTaskByRequest').and.returnValue(of(fakeList));
-    spyOn(processListCloudService, 'getProcessByRequest').and.returnValue(of(fakeList));
-    fixture.detectChanges();
   });
 
   it('should create', () => {
