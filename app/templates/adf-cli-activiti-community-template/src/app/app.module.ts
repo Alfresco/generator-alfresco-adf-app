fe7564746831d9374fa56ef2113a62442702f888
Denys Vuika <denys.vuika@gmail.com>
2022-11-09 17:12:17 +0000

    adf-cli-activiti-community-template: ng-14

--- app/templates/adf-cli-activiti-community-template/src/app/app.module.ts
+++ app/templates/adf-cli-activiti-community-template/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { ApolloModule } from 'apollo-angular';
 
 
 // ADF modules
@@ -33,10 +34,9 @@ import { StartTaskComponent } from './start-task/start-task.component';
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        RouterModule.forRoot(
-            appRoutes // ,
-            // { enableTracing: true } // <-- debugging purposes only
-        ),
+        RouterModule.forRoot(appRoutes // ,
+// { enableTracing: true } // <-- debugging purposes only
+, { relativeLinkResolution: 'legacy' }),
 
         // ADF modules
         CoreModule.forRoot(),
@@ -45,7 +45,8 @@ import { StartTaskComponent } from './start-task/start-task.component';
         TranslateModule.forRoot({
             loader: { provide: TranslateLoader, useClass: TranslateLoaderService }
         }),
-        StencilsModule
+        StencilsModule,
+        ApolloModule
     ],
     declarations: [
         AppComponent,
